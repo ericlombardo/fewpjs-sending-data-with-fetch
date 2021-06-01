5ceb35eda3009f1f4cb19a45adcb3592d2cdd788
Eric <eric.m.lombardo@gmail.com>
2021-06-01 15:08:18 -0400

    commit tree ac1bf4636e6f58cb5d06c7c40318ad3616d07890

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email){
+  const action = 'http://localhost:3000/users';
+  let formData = { name: name, email: email };
+  
+  let configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch(action, configObj)
+  .then(resp => resp.json())
+  .then(jresp => AppendData(jresp))
+  .catch((error) => handleError(error))
+}
+
+function AppendData(jresp){
+  let id = jresp.id;
+  document['body'].append(id);
+}
+function handleError(error){
+  document.body.append(error.message);
+}
+
